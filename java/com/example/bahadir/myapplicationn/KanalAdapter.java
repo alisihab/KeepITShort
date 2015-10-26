b655b053344bb3edc8aff0f7d090d6821bf28533
alisihab <sihab.akcan@gmail.com>
2015-10-26 17:27:48 +0200

    New changelist

--- java/com/example/bahadir/myapplicationn/KanalAdapter.java
+++ java/com/example/bahadir/myapplicationn/KanalAdapter.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 
 
 public class KanalAdapter extends BaseAdapter {
-    String deneme;
+    String denemeasd;
     Context context;
     String charset;
     String query;
